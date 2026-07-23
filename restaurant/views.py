27d190c4c8a1f0cd93f3c054398cdbba865677a5
Amit Kumar <sinha.amit22082000@gmail.com>
2026-07-23 06:17:35 +0530

    Add Django REST Framework APIs for menu and booking

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuItemSerializer
+from .models import Booking
+from .serializers import BookingSerializer
+
+from .serializers import UserSerializer
 
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
